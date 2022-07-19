1fd2658b2cea039a4080e029130fdb194c6dd15b
yalem123 <yalembrhankelayneh@gmail.com>
2022-07-19 10:42:21 -0700

    task2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,9 +7,9 @@ class square which defines private attribute
 class square:
     """ class square which defines private attribute """
     def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
+        if (type(size) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (size < 0):
+            raise (ValueError("size must be >= 0"))
         else:
             self.__size = size
