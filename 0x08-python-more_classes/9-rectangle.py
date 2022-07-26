43d4a4a1dd45dac186f42bcd958b9b0c149c68ad
yalem123 <yalembrhankelayneh@gmail.com>
2022-07-26 01:39:09 -0700

    submit

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -38,27 +38,27 @@ class Rectangle():
 
         return (self.__width * 2) + (self.__height * 2)
 
-    def __str__(self):
-        """
-        Function to print a Square with the print_symbol
-        """
+     def __str__(self):
+         """
+         Function to print a Square with the print_symbol
+         """
 
-        if self.__width == 0 or self.__height == 0:
-            return ""
+         if self.__width == 0 or self.__height == 0:
+             return ""
 
-        final = [str(self.print_symbol) * self.__width
-                 for character in range(self.__height)]
+         final = [str(self.print_symbol) * self.
+                  for character in range(self.__height)]
 
-        return '\n'.join(final)
+         return '\n'.join(final)
 
-    def __repr__(self):
-        """Returns an “official” string representation of a Rectangle"""
-        return f'Rectangle({self.__width}, {self.__height})'
+     def __repr__(self):
+         """Returns an “official” string representation of a Rectangle"""
+         return f'Rectangle({self.__width}, {self.__height})'
 
-    def __del__(self):
-        """Prints a message when a Rectangle instance is deleted"""
-        print('Bye rectangle...')
-        Rectangle.number_of_instances -= 1
+     def __del__(self):
+         """Prints a message when a Rectangle instance is deleted"""
+         print('Bye rectangle...')
+         Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
@@ -80,45 +80,45 @@ class Rectangle():
 
         self.__width = value
 
-        @property
-        def height(self):
-            """Getter of the property height"""
-            return self.__height
+    @property
+    def height(self):
+        """Getter of the property height"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """
-            Getter of the property value
-              Args:
-                - value: int
-            """
-            if not isinstance(value, int):
-                raise TypeError('height must be an integer')
+    @height.setter
+    def height(self, value):
+        """
+        Getter of the property value
+          Args:
+            - value: int
+        """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
 
-            if value < 0:
-                raise ValueError('height must be >= 0')
+        if value < 0:
+            raise ValueError('height must be >= 0')
 
-            self.__height = value
+        self.__height = value
 
-        @staticmethod
-        def bigger_or_equal(rect_1, rect_2):
-            """
-            Return the biggest rectangle based on the area
-              Args:
-                - rect_1: Rectangle
-                - rect_2: Rectangle
-            """
-            if not isinstance(rect_1, Rectangle):
-                raise TypeError("rect_1 must be an instance of Rectangle")
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """
+        Return the biggest rectangle based on the area
+          Args:
+            - rect_1: Rectangle
+            - rect_2: Rectangle
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
 
-            if not isinstance(rect_2, Rectangle):
-                raise TypeError("rect_2 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
 
-            if Rectangle.area(rect_1) >= Rectangle.area(rect_2):
-                return rect_1
-            return rect_2
+        if Rectangle.area(rect_1) >= Rectangle.area(rect_2):
+            return rect_1
+        return rect_2
 
-        @classmethod
-        def square(cls, size=0):
-            """Returns a new Rectangle instance with width == height == size"""
-            return cls(size, size)
+    @classmethod
+    def square(cls, size=0):
+        """Returns a new Rectangle instance with width == height == size"""
+        return cls(size, size)
