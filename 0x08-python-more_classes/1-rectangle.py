1365a09e58407e208dd6c600408aa52aab25d1bb
yalem123 <yalembrhankelayneh@gmail.com>
2022-07-25 19:08:14 -0700

    submit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,58 +1,10 @@
 #!/usr/bin/python3
-"""This is a class rectangle that defines attributes"""
+"""Class Square with size"""
 
 
-class Rectangle():
-    """
-    A Rectangle Class with the private instance attributes width and height
-    """
+class Square:
+    """Class to create a square with it size"""
 
-    def __init__(self, width=0, height=0):
-        """
-        Constructor of the class Rectangle
-        Args:
-        width (default = 0): int
-        height (default = 0): int
-        """
-        self.height = height
-        self.width = width
-
-        @property
-        def width(self):
-            """Getter of the property width"""
-            return self.__width
-
-        @width.setter
-        def width(self, value):
-            """
-            Getter of the property value
-            Args:
-                value: int
-            """
-            if not isinstance(value, int):
-                raise TypeError('width must be an integer')
-
-            if value < 0:
-                raise ValueError('width must be >= 0')
-
-            self.__width = value
-
-            @property
-            def height(self):
-                """Getter of the property height"""
-                return self.__height
-
-            @height.setter
-            def height(self, value):
-                """
-                Getter of the property value
-                Args:
-                    value: int
-                """
-                if not isinstance(value, int):
-                    raise TypeError('height must be an integer')
-
-                if value < 0:
-                    raise ValueError('height must be >= 0')
-
-                self.__height = value
+    def __init__(self, square_size):
+        """Constructor of the Square"""
+        self.__size = square_size
