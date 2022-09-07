b2869d4c666d7530d253d4361039526e25530c08
yalem123 <yalembrhankelayneh@gmail.com>
2022-09-07 16:58:00 -0700

    submit

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- Write a script that creates the database hbtn_0d_2
+-- grant select privileges on hbtn_0d_2.* to user_0d_2@localhost.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'user_0d_2_pwd';
