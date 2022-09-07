52437a0b8e1c51bc45d975bb81fe1d6d2e5ae3e9
yalem123 <yalembrhankelayneh@gmail.com>
2022-09-07 05:16:35 -0700

    submit

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- displays the number of records with in the table first_table of the database.
+-- query of the number of records with in the table first_table of the database.
+SELECT COUNT(*) as id FROM first_table WHERE id=89;
