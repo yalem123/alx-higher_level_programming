89b04e3dd14f095aadca36e641e721bf1ef8827c
yalem123 <yalembrhankelayneh@gmail.com>
2022-09-07 05:45:28 -0700

    submit

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- print a script that computes the score average of all records in the table second_table
+-- query of a script that computes the score average of all records in the table second_table
+SELECT AVG(score) AS average FROM second_table;
