379292ce1db141b1ad81429030266a8338cdd4d9
yalem123 <yalembrhankelayneh@gmail.com>
2022-09-07 05:12:16 -0700

    submit

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- inserts a new row in the table first_table
 -- query for inserts a new row in the table of mysql.
-INSERT INTO first_table(id, name) VALUES (89, Best School);
+INSERT INTO first_table(id, name) VALUES (89, "Best School");
