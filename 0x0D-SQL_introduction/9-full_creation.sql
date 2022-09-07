71f083ae9392a8ceff683137c484820f28e83ce1
yalem123 <yalembrhankelayneh@gmail.com>
2022-09-07 05:22:52 -0700

    submit

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- prints script that creates a table second_table in the database.
+-- query for creating a table second_table in the database hbtn_0c_0.
+CREATE TABLE second_table(id INT, name VARCHAR(256), score INT);
