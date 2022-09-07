5d9f854a14ce16a1b471b7676b2cee87b571223c
yalem123 <yalembrhankelayneh@gmail.com>
2022-09-07 05:07:13 -0700

    submit

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- prints a script that inserts a new row in the table of mysql.
+-- query for script that inserts a new row in the table of mysql.
+INSERT INTO first_table(id, name) VALUES (89, Best School);
