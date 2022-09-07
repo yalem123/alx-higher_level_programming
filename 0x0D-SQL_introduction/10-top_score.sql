8b37162205a3da5a0758fa6fa3c5402c56c1cfda
yalem123 <yalembrhankelayneh@gmail.com>
2022-09-07 05:35:06 -0700

    submit

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- print a script that lists all records of the table second_table of the database hbtn_0c_0
+-- query of script that lists all records of the table second_table
+SELECT score, name FROM second_table ORDER BY score DESC;
