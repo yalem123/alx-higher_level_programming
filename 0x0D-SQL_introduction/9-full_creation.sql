4e58e035c3356da12b0fcc8ae3faed2b4f78ae32
yalem123 <yalembrhankelayneh@gmail.com>
2022-09-07 05:29:09 -0700

    submit

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,7 @@
 -- prints script that creates a table second_table in the database.
 -- query for creating a table second_table in the database hbtn_0c_0.
-CREATE TABLE second_table(id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 4);
+INSERT INTO second_table (id, name, score) VALUES (4, "Goerge", 8);
