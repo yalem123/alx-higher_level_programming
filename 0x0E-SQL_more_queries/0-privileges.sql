10388e186987f5c1e6fe38507cc3bb8f2eefc8db
yalem123 <yalembrhankelayneh@gmail.com>
2022-09-07 16:20:02 -0700

    submit

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- print a script that lists all privileges of the MySQL users
+-- query of a script that lists all privileges of the MySQL user
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
