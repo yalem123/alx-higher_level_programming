6a89e3a3bfa51f962adc5f1a885b29d5e45a0fa2
yalem123 <yalembrhankelayneh@gmail.com>
2022-09-07 05:48:15 -0700

    submit

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- print a script that lists the number of records with the same score in the table
+-- query of a script that lists the number of records with the same score in the table
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY score DESC;
