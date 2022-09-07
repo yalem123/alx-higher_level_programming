82ebef7c511e3b4cb4110697020f2fcfe0e24565
yalem123 <yalembrhankelayneh@gmail.com>
2022-09-07 05:43:22 -0700

    submit

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- print a script that updates the score of Bob to 10
+-- query of a script that updates the score of Bob to 10
+UPDATE second_table;
+SET score = 10;
+WHERE name = "Bobi";
---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- print a script that removes all records with a score <= 5 in the table
+-- query of a script that removes all records with a score <= 5 in the table
+DELETE FROM second_table WHERE score <= 5;
