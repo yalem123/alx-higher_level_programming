d588f903665db1a48ee1b89049f9522a124bf1a6
yalem123 <yalembrhankelayne@gmail.com>
2022-10-04 01:39:31 +0000

    submit

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const count = process.argv.length;
+if (count === 2) {
+  console.log('No argument');
+} else if (count === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
+}
