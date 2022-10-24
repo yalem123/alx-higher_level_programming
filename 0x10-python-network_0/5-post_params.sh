5bd406f2c08c5f212b6f0a29b0e92c908e3b6473
yalem123 <yalembrhankelayneh@gmail.com>
2022-10-24 14:40:27 -0700

    submit

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash scripts that sends a POST request to a given URL.
-curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" "${1}"
+curl -sX "email=test@gmail.com&subject=I will always be here for PLD" "${1}"
